473d716e8a09896860b3f6cf2238785ddcb4fb70
Ferris Thiel <ferris.thiel@icloud.com>
2026-06-10 09:27:07 +0200

    Replace broken catalog with tiny safe data

--- src/data/catalog.js
+++ src/data/catalog.js
@@ -1,10 +1,7 @@
-export const apps = [
-  { id: 'store', type: 'app', title: 'Store', tag: 'Aurora App', a: '#efe8cf', b: '#fff' },
-  { id: 'library', type: 'app', title: 'Library', tag: 'Collection', a: '#dde8f5', b: '#fff' },
-  { id: 'photos', type: 'app', title: 'Aurora Photos', tag: 'Media', a: '#f6e7b8', b: '#fff' },
-]
-
-export const games = [
-  { id: 'spider1', type: 'game', title: 'Spider-Man', tag: 'Action', a: '#07111f', b: '#1f2937' },
-  { id: 'reddead', type: 'game', title: 'Red Dead Redemption III', tag: 'Adventure', a: '#dc2626', b: '#f59e0b' },
-  { id: 'ferris', type: 'game', title: 'Ferris Game', tag:
+export const items=[
+{id:'store',type:'app',title:'Store',tag:'App',a:'#efe8cf',b:'#fff'},
+{id:'library',type:'app',title:'Library',tag:'Collection',a:'#dde8f5',b:'#fff'},
+{id:'photos',type:'app',title:'Aurora Photos',tag:'Media',a:'#f6e7b8',b:'#fff'},
+{id:'ferris',type:'game',title:'Ferris Game',tag:'Installed',url:'https://ferristhiel.github.io/Game/',a:'#20242c',b:'#111827'},
+{id:'reddead',type:'game',title:'Red Dead Redemption III',tag:'Adventure',a:'#dc2626',b:'#f59e0b'},
+{id:'last',type:'game',title:'The Last of Us',tag:'Story',
