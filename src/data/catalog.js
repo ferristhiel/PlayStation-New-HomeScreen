135aeab5ae7d8e871001bc046c7ddc6bbec74a93
Ferris Thiel <ferris.thiel@icloud.com>
2026-06-10 09:31:14 +0200

    Fix tiny complete catalog

--- src/data/catalog.js
+++ src/data/catalog.js
@@ -1,7 +1,6 @@
-export const items=[
-{id:'store',type:'app',title:'Store',tag:'App',a:'#efe8cf',b:'#fff'},
-{id:'library',type:'app',title:'Library',tag:'Collection',a:'#dde8f5',b:'#fff'},
-{id:'photos',type:'app',title:'Aurora Photos',tag:'Media',a:'#f6e7b8',b:'#fff'},
-{id:'ferris',type:'game',title:'Ferris Game',tag:'Installed',url:'https://ferristhiel.github.io/Game/',a:'#20242c',b:'#111827'},
-{id:'reddead',type:'game',title:'Red Dead Redemption III',tag:'Adventure',a:'#dc2626',b:'#f59e0b'},
-{id:'last',type:'game',title:'The Last of Us',tag:'Story',
+export const items = [
+  { id: 'store', type: 'app', title: 'Store', tag: 'App', a: '#efe8cf', b: '#fff' },
+  { id: 'ferris', type: 'game', title: 'Ferris Game', tag: 'Installed', a: '#20242c', b: '#111827' },
+  { id: 'reddead', type: 'game', title: 'Red Dead', tag: 'Adventure', a: '#dc2626', b: '#f59e0b' },
+  { id: 'last', type: 'game', title: 'The Last of Us', tag: 'Story', a: '#8b735f', b: '#262626' }
+]
