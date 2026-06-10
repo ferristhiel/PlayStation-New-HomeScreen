14a4228e617d56461c440ecfb2fc8a0663ddf86c
Ferris Thiel <ferris.thiel@icloud.com>
2026-06-10 09:07:12 +0200

    Restore complete working console app

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import'./ux-fixes.css'
 
 const CLIENT_ID='d06ce2e44ce944c18f421e373e2b086a'
 const REDIRECT_URI=`${window.location.origin}${window.location.pathname}`
-const TOKEN_KEY='aurora.spotify.token.v4'
-const VERIFIER_KEY='aurora.spotify.verifier.v4'
-const SETTINGS_KEY='aur
+const TOKEN_KEY='aurora.spotify.token.v5'
+const VERIFIER_KEY='aurora.spotify.verifier.v5'
+const SETTINGS_KEY='aurora.settings.v5'
+const defaultSettings={theme:'dark',carousel:'oval',volume:68,compact:false,reduceMotion:false,menuScale:100}
