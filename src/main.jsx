587d8e00b9f0717783bd95727e9ebe3370345286
Ferris Thiel <ferris.thiel@icloud.com>
2026-06-10 09:16:42 +0200

    Repair app with complete compact implementation

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,3 @@
-import React,{useEffect,useMemo,useState}from'react'
-import{createRoot}from'react-dom/client'
-import'./styles.css'
-import'./ux-fixes.css'
-
-const CLIENT_ID='d06ce2e44ce944c18f421e373e2b086a'
-const REDIRECT_URI=`${window.location.origin}${window.location.pathname}`
-const TOKEN_KEY='aurora.spotify.token.v6'
-const VERIFIER_KEY='aurora.spotify.verifier.v6'
-const SETTINGS_KEY='aurora.settings.v6'
-const defaultSettings={theme:'dark',carousel:'oval',volume:68,compact:false,reduceMotion:false,menuScale:100}
-const tiles=[
-{id:'store',type:'app',title:'Store',tag:'Aurora App',a:'#eee8d8',b:'#f8f3e7'},
-{id:'library',type:'app',title
+import React,{useEffect,useState}from'react';import{createRoot}from'react-dom/client';import'./styles.css';import'./ux-fixes.css';
+const ID='d06ce2e44ce944c18f421e373e2b086a',REDIR=location.origin+location.pathname,TK='aurora.tk',VF='aurora.vf',SK='aurora.set';
+const tiles=['Store','Library','Aurora Photos','Spider-Man','Red Dead Redemption III','Ferris Game','The Last of Us','The Last of Us Part II','FC 27','Spider-Man 3','GTA','Cyberpunk 2'].map((t,i)=>({id:t.toLowerCase().replaceAll(' ','-'),title:t,tag:i<3?'App':i==5?'Installed':'Game',a:['#eee
