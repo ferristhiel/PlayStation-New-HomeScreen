a693fc40ac4d2c9eebc5ce9db365e5d77375829b
Ferris Thiel <ferris.thiel@icloud.com>
2026-06-11 09:29:31 +0200

    Restore small React entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 import './styles/base.css'
 
-function App() {
-  const items = ['Store','Library','Photos','Spider','Ferris','FC27']
-  return (
-    <main className="app">
-      <header className="top">
-        <div className="left">12:07 ))) H</div>
-        <div className="music">Spotify Player · Connect</div>
-        <div className="right">FT Settings</div>
-      </header>
-      <section className="hero
+createRoot(document.getElementById('root')).render(<App />)
